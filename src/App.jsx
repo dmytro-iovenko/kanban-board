8db45cae7d22121bbde17407212441527922db3f
Dmytro Iovenko <iovenko@gmail.com>
2024-10-03 21:59:36 -0400

    added env variables for jira connection

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,11 @@ import initialTasks from "./data/initialData";
 import KanbanBoard from "./components/KanbanBoard";
 import { Box } from "@mui/material";
 
+const JIRA_API_URL=import.meta.env.VITE_JIRA_API_URL
+const JIRA_USERNAME=import.meta.env.VITE_JIRA_USERNAME
+const JIRA_API_KEY=import.meta.env.VITE_JIRA_API_KEY
+
+console.log(JIRA_API_URL, JIRA_USERNAME, JIRA_API_KEY)
 const App = () => {
   const statuses = ["todo", "in-progress", "done"];
   const groupedTasks = statuses.reduce((acc, status) => {
